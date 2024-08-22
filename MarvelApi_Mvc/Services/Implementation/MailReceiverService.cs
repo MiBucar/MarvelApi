42c4fb6aa8105e60178501d56a24b7cc45a58c79
Swathed Orange <mislavbucar@gmail.com>
2024-08-22 12:31:20 +0200

    Added About, Contact page and mail service

---
+++ MarvelApi_Mvc/Services/Implementation/MailReceiverService.cs
@@ -0,0 +1,46 @@
+﻿using MailKit;
+using MarvelApi_Mvc.Models.DTOs.UserDTOs;
+using MarvelApi_Mvc.Services.IServices;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System.Net.Mail;
+
+namespace MarvelApi_Mvc.Services.Implementation
+{
+    public class MailReceiverService : IMailReceiverService
+    {
+        private const string EmailAdress = "mislavbucar@gmail.com";
+
+        public async Task SendEmailAsync(MailDTO mailDto)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(mailDto.Name, mailDto.Email));
+            email.To.Add(MailboxAddress.Parse(EmailAdress));
+            email.Subject = mailDto.Subject;
+            email.Body = new TextPart("plain")
+            {
+                Text = $"{mailDto.Email} \n {mailDto.Comment}"
+            };
+
+            await ConnectAndSendEmailAsync(email);
+        }
+
+        private async Task ConnectAndSendEmailAsync(MimeMessage email)
+        {
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                try
+                {
+                    smtp.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                    smtp.Authenticate(EmailAdress, "kotf vwfj uasv daoy");
+                    await smtp.SendAsync(email);
+                    smtp.Disconnect(true);
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ MarvelApi_Mvc/ViewComponents/ContactFormViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using MarvelApi_Mvc.Models.DTOs.UserDTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarvelApi_Mvc.ViewComponents
+{
+    public class ContactFormViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new MailDTO();
+            return View(model);
+        }
+    }
+}
